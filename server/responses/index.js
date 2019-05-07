e3c13b47db3302da0935986771d9e0d1899b62a5
Vinay Shrestha <vinayshrestha555@gmail.com>
2019-05-07 20:53:12 +0545

    api responses helper added

--- server/responses/index.js
+++ server/responses/index.js
@@ -1,20 +1,33 @@
 module.exports = {
-    success: (res, opts = {}) => {
-        const message = opts.message || 'Success';
-        const data = opts.data || {};
-        return res.send({ status: 200, message, data });
-    },
+   badRequest: (res, opts = {}) => {
+      const message = opts.message || 'bad_request';
+      const data = opts.data || {};
+      return res.send({ status: 400, message, error: data });
+   },
 
-    badRequest: (res, opts = {}) => {
-        const message = opts.message || 'Bad Request';
-        const data = opts.data || {};
-        return res.send({ status: 400, message, error: data });
-    },
+   invalidToken: (res, opts = {}) => {
+      const message = opts.message || 'token_invalid';
+      const data = opts.data || {};
+      return res.send({ status: 401, message, error: data });
+   },
 
-    serverError: (res, opts = {}) => {
-        const message = opts.message || 'Server Error';
-        const data = opts.data || {};
-        return res.send({ status: 500, message, error: data });
-    },
+   serverError: (res, opts = {}) => {
+      const message = opts.message || 'server_error';
+      const data = opts.data || {};
+      return res.send({ status: 500, message, error: data });
+   },
+
+   success: (res, opts = {}) => {
+      const message = opts.message || 'success';
+      const data = opts.data || {};
+      return res.send({ status: 200, message, data });
+   },
+
+
+   unauthorized: (res, opts = {}) => {
+      const message = opts.message || 'unauthorized';
+      const data = opts.data || {};
+      return res.send({ status: 403, message, error: data });
+   },
 
 }
