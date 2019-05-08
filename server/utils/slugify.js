98baf63958a6565c915b37d2c1b483b61b96696a
Vinay Shrestha <vinayshrestha555@gmail.com>
2019-05-09 01:10:39 +0545

    slugify function modified

--- server/utils/slugify.js
+++ server/utils/slugify.js
@@ -1,5 +1,9 @@
 const slugify = require('slugify');
 
+/**
+ * @param {string} name Name to generate the slug
+ * @returns {string} Returns the generated slug
+ */
 module.exports = (name) => {
    return slugify(name, {
       replacement: '-',
