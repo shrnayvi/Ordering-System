ba81a8e0f177bbc29d388820ef6e55643345cd65
Vinay Shrestha <vinayshrestha555@gmail.com>
2019-05-19 15:44:25 +0545

    filename renamed

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 
-import Routes from './routes';
+import Routes from './Routes';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import history from './helpers/history';
